ebf5a4f88ada91a2e8a15b82d2bc75581c7f7ce3
KultumL <klhabai@emory.edu>
2026-02-22 16:41:19 -0500

    fixed 404 error

--- frontend/glucose-ml-web/src/App.tsx
+++ frontend/glucose-ml-web/src/App.tsx
@@ -1,19 +1,19 @@
 import HomePage from "./components/HomePage";
 import AppLayout from "./components/AppLayout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import DatasetDetail from "./components/dataset_detail/DatasetDetail";
 
 // Main app with routing
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/dataset/:id" element={<DatasetDetail />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
