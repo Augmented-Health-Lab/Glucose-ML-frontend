c483700f76b702bfa6b629222c36476e64d30707
KultumL <klhabai@emory.edu>
2026-03-01 14:37:17 -0500

    Update PageTitle.tsx

--- frontend/glucose-ml-web/src/components/PageTitle.tsx
+++ frontend/glucose-ml-web/src/components/PageTitle.tsx
@@ -5,7 +5,7 @@ const PageTitle = ({ title, text }: { title: string; text: string }) => {
   const stats = [
     { value: "20+", label: "Datasets" },
     { value: "4,393", label: "Participants" },
-    { value: "33,7984", label: "Days of CGM" },
+    { value: "337,984", label: "Days of CGM" },
     { value: "44.9M", label: "Glucose samples" },
   ];
 
